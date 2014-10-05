cd1598475c751c60fd4da9c7746b8ead0c829000
David Graunke <graunked@gmail.com>
2014-10-05 09:31:39 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular
+  .module('scrollviewTestApp', ['famous.angular']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('scrollviewTestApp')
+  .controller('MainCtrl', function ($scope, $famous) {
+
+    var EventHandler = $famous["famous/core/EventHandler"];
+
+  });
